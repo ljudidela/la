f3e4065f413246091c49a407f1371cc4015a1ad0
tgbfb <144910828+tgbfb@users.noreply.github.com>
2026-01-01 19:52:10 +0300

    AI: create src/data/cameras.ts — Создать приложение

---
+++ src/data/cameras.ts
@@ -0,0 +1,64 @@
+import { Camera } from '../types';
+
+export const CAMERAS: Camera[] = [
+  {
+    id: '1',
+    name: 'Santa Monica Pier',
+    location: 'Santa Monica, CA',
+    url: 'https://www.youtube.com/watch?v=im2Gj5-Vq0A',
+    thumbnail: 'https://img.youtube.com/vi/im2Gj5-Vq0A/maxresdefault.jpg',
+    lat: 34.0092,
+    lng: -118.4976,
+    type: 'youtube'
+  },
+  {
+    id: '2',
+    name: 'Venice Beach Boardwalk',
+    location: 'Venice, CA',
+    url: 'https://www.youtube.com/watch?v=yy8FSCX8K0g',
+    thumbnail: 'https://img.youtube.com/vi/yy8FSCX8K0g/maxresdefault.jpg',
+    lat: 33.9850,
+    lng: -118.4695,
+    type: 'youtube'
+  },
+  {
+    id: '3',
+    name: 'Hollywood Blvd',
+    location: 'Hollywood, CA',
+    url: 'https://www.youtube.com/watch?v=u_J8wM-Wb0k',
+    thumbnail: 'https://img.youtube.com/vi/u_J8wM-Wb0k/maxresdefault.jpg',
+    lat: 34.1016,
+    lng: -118.3337,
+    type: 'youtube'
+  },
+  {
+    id: '4',
+    name: 'West Hollywood Skyline',
+    location: 'West Hollywood, CA',
+    url: 'https://www.youtube.com/watch?v=DnRj7fN9Rz0',
+    thumbnail: 'https://img.youtube.com/vi/DnRj7fN9Rz0/maxresdefault.jpg',
+    lat: 34.0900,
+    lng: -118.3617,
+    type: 'youtube'
+  },
+  {
+    id: '5',
+    name: 'LAX Airport',
+    location: 'Los Angeles, CA',
+    url: 'https://www.youtube.com/watch?v=B1L6jQn_qM0',
+    thumbnail: 'https://img.youtube.com/vi/B1L6jQn_qM0/maxresdefault.jpg',
+    lat: 33.9416,
+    lng: -118.4085,
+    type: 'youtube'
+  },
+  {
+    id: '6',
+    name: 'Beverly Hills - Rodeo Dr',
+    location: 'Beverly Hills, CA',
+    url: 'https://www.youtube.com/watch?v=M-N0qC7e74A',
+    thumbnail: 'https://img.youtube.com/vi/M-N0qC7e74A/maxresdefault.jpg',
+    lat: 34.0696,
+    lng: -118.4053,
+    type: 'youtube'
+  }
+];
